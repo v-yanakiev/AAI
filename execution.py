00a73e8146fea41d59fb7bc51348b5d2f6078788
v-yanakiev <vaskoleon@gmail.com>
2022-05-25 02:29:45 +0300

    works

---
+++ execution.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from fft import FastFourierTransform
+
+samplingFrequency =2**20
+tstep=1/samplingFrequency
+frequency=128
+numberOfSamples=int(samplingFrequency/frequency)
+timeSteps=np.linspace(0,(numberOfSamples-1)*tstep,numberOfSamples)
+fstep=samplingFrequency/numberOfSamples
+f=np.linspace(0,(numberOfSamples-1)*fstep,numberOfSamples)
+signal=1*np.sin(2*np.pi*frequency*timeSteps)+1*np.sin(10*np.pi*frequency*timeSteps)
+
+fftResult = FastFourierTransform(signal)
+fftResult_mag=np.abs(fftResult)/len(signal)
+
+f_plot = f[0:int(numberOfSamples/2+1)]
+f_plot_mag = 2*fftResult_mag[0:int(numberOfSamples/2+1)]
+f_plot_mag[0] = f_plot_mag[0]/2
+
+fig, [ax1, ax2]=plt.subplots(nrows=2,ncols=1)
+ax1.plot(timeSteps,signal,'.-')
+ax2.plot(f_plot,f_plot_mag,'.-')
+
+plt.show()
--- fft.py
+++ fft.py
@@ -2,6 +2,7 @@ import numpy as np;
 import matplotlib as mpl;
 import matplotlib.pyplot as plt;
 def FastFourierTransform(vectorForWhichToCompute):
+    vectorForWhichToCompute=np.asarray(vectorForWhichToCompute,dtype=float)
     vectorSize = vectorForWhichToCompute.shape[0]
     if vectorSize % 2 > 0:
         raise ValueError("Vector size must be a power of 2!")
@@ -14,33 +15,34 @@ def FastFourierTransform(vectorForWhichToCompute):
         return np.concatenate([even + terms[:int(vectorSize/2)] * odd,
                                even + terms[int(vectorSize/2):] * odd])
 def DiscreteFourierTransform(vectorForWhichToCompute):
+    vectorForWhichToCompute=np.asarray(vectorForWhichToCompute,dtype=float)
     vectorSize = vectorForWhichToCompute.shape[0]
     generated = np.arange(vectorSize)
     new = generated.reshape((vectorSize, 1))
     toMultiply = np.exp(-2j * np.pi * new * generated / vectorSize)
     return np.dot(toMultiply, vectorForWhichToCompute)
-def generateSeries(freq,vectorSize):
-    return np.array([np.sin(2*np.pi*item*freq)+np.sin(2*np.pi*item*(freq+100)) for item in range(vectorSize)])
-# randomArray = np.array([np.sin(2*np.pi*item*1/10)+np.sin(2*np.pi*item/3)+np.random.rand()-0.5 for item in range(1024)])
-freq=1/100
-vectorSize=128
-randomArray = generateSeries(freq,vectorSize)
-# result = np.sort(FastFourierTransform(randomArray));
+# def generateSeries(freq,vectorSize):
+#     return np.array([np.sin(2*np.pi*item*freq)+np.sin(2*np.pi*item*(freq+100)) for item in range(vectorSize)])
+# # randomArray = np.array([np.sin(2*np.pi*item*1/10)+np.sin(2*np.pi*item/3)+np.random.rand()-0.5 for item in range(1024)])
+# freq=1/100
+# vectorSize=128
+# randomArray = generateSeries(freq,vectorSize)
+# # result = np.sort(FastFourierTransform(randomArray));
 
 
-plt.title("Before FFT")
-plt.subplot(1, 2, 1)
-plt.xlabel('Time')
-plt.ylabel('Intensity')
-plt.plot(range(randomArray.shape[0]),randomArray)
-fftResult = FastFourierTransform(randomArray)
+# plt.title("Before FFT")
+# plt.subplot(1, 2, 1)
+# plt.xlabel('Time')
+# plt.ylabel('Intensity')
+# plt.plot(range(randomArray.shape[0]),randomArray)
+# fftResult = FastFourierTransform(randomArray)
 
-plt.subplot(1, 3, 3)
-# plt.xlim(0,20);
-plt.xlabel('Frequency')
-plt.ylabel('Amplitude')
-toPlot=(2.0/vectorSize)*np.abs(fftResult[:vectorSize//2]) 
-plt.plot(range(fftResult.shape[0]),fftResult);
+# plt.subplot(1, 3, 3)
+# # plt.xlim(0,20);
+# plt.xlabel('Frequency')
+# plt.ylabel('Amplitude')
+# toPlot=(2.0/vectorSize)*np.abs(fftResult[:vectorSize//2]) 
+# plt.plot(range(fftResult.shape[0]),fftResult);
 
-plt.show();
-# print(np.allclose(FastFourierTransform(randomArray), np.fft.fft(randomArray)))
+# plt.show();
+# # print(np.allclose(FastFourierTransform(randomArray), np.fft.fft(randomArray)))
